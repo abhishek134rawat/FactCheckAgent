e94c095055a69945aa94561330a6d6b9307f28dd
abhishek134rawat <Abhishekrawat988@gmail.com>
2026-06-18 23:45:33 +0530

    fic gemini model

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("gemini-1.5-flash-latest")
+model = genai.GenerativeModel("gemini-1.5-pro")
 
 st.title("Fact Check Agent")
 
